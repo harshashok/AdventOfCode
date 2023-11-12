31f02ab0f9ab4c80fdc75b7c8c4faaefe68b2a42
hashok <hars.ashok@gmail.com>
2023-11-11 18:43:25 -0800

    Day 2022/13 : Solution Part 2

--- 2022/Day13/Solution.cs
+++ 2022/Day13/Solution.cs
@@ -13,45 +13,42 @@ namespace AdventOfCode.Y2022.Day13;
 [ProblemName("Distress Signal")]      
 class Solution : Solver {
     List<Pairs> pairs = new();
-    static bool DEBUG = false;
 
     public object PartOne(string input) {
         ReadInput(input);
-        //int element = 1;
-        //var pair = pairs.ElementAt(element);
-        //Console.WriteLine($"== Pair {element + 1} ==");
-        //return Evaluate(pair.left, pair.right);
-
-
-        //var rrr = pairs.Select((pair, index) =>
-        //{
-        //    //Console.WriteLine($"== Pair {index + 1} ==");
-        //    var retVal = Evaluate(pair.left, pair.right);
-        //    //Console.WriteLine();
-        //    return retVal == Result.CorrectOrder ? index + 1 : 0;
-        //});
-
-        //return rrr.Sum();
-
-        return pairs.Select((pairs, index) => Evaluate(pairs.left, pairs.right) == Result.CorrectOrder ? index + 1 : 0).Sum();
-
-        //return pairs.Select((pair, index) => Compare(pair.left, pair.right) < 0 ? index + 1 : 0).Sum();
+        return pairs.Select((pairs, index) =>
+            Evaluate(pairs.left, pairs.right) == Result.CorrectOrder ? index + 1 : 0).Sum();
     }
 
     public object PartTwo(string input) {
-        return 0;
+        var divider2 = JsonNode.Parse("[[2]]");
+        var divider6 = JsonNode.Parse("[[6]]");
+        var list = ReadInputPart2(input)
+            .Append(divider2)
+            .Append(divider6)
+            .ToList();
+
+        list.Sort(CompareTo);
+
+        return (list.IndexOf(divider2)+1) * (list.IndexOf(divider6)+1);
     }
 
+    private int CompareTo(JsonNode left, JsonNode right)
+    {
+        return Evaluate(left, right) switch
+        {
+            Result.CorrectOrder => -1,
+            Result.WrongOrder => 1,
+            _ => 0
+        };
+    }
 
     private Result Evaluate(JsonNode left, JsonNode right, int indent = 0)
     {
-        Log(left.ToJsonString(), right.ToJsonString(), indent+1);
-
         if(left is JsonValue leftVal && right is JsonValue)
         {
-            //if ((int)left == (int)right) return Result.Continue;
-            if ((int)left < (int)right) { if(DEBUG) Console.WriteLine("Left side is smaller, so inputs are in the right order"); return Result.CorrectOrder; }
-            if ((int)left > (int)right) { if(DEBUG) Console.WriteLine("Right side is smaller, so inputs are not in the right order"); return Result.WrongOrder; }
+            if ((int)left < (int)right) { return Result.CorrectOrder; }
+            if ((int)left > (int)right) { return Result.WrongOrder; }
             return Result.Continue;
         }
 
@@ -68,40 +65,9 @@ class Solution : Solver {
             }
         }
 
-        if (arrayA.Count < arrayB.Count) { if(DEBUG) Console.WriteLine("Left side ran out of items, so inputs are in the right order"); }
-        if (arrayA.Count > arrayB.Count) { if(DEBUG) Console.WriteLine("Right side ran out of items, so inputs are not in the right order"); }
         return arrayA.Count < arrayB.Count ? Result.CorrectOrder :
             (arrayA.Count == arrayB.Count) ? Result.Continue : Result.WrongOrder;
 
-    }
-
-    //to delete.
-    int Compare(JsonNode left, JsonNode right)
-    {
-        if (left is JsonValue leftVal && right is JsonValue rightVal)
-        {
-            return left.GetValue<int>() - right.GetValue<int>();
-        }
-
-        var leftArray = left switch { JsonArray a => a, _ => new JsonArray(left.GetValue<int>()) };
-        var rightArray = right switch { JsonArray a => a, _ => new JsonArray(right.GetValue<int>()) };
-        foreach (var (leftItem, rightItem) in Enumerable.Zip(leftArray, rightArray))
-        {
-            var c = Compare(leftItem, rightItem);
-            if (c != 0)
-            {
-                return c;
-            }
-        }
-        return leftArray.Count - rightArray.Count;
-    }
-
-    //to delete
-    private Result tempCompare(JsonArray arrayA, JsonArray arrayB)
-    {
-        if (arrayA.Count <= arrayB.Count) Console.WriteLine("Left side ran out of items, so inputs are in the right order");
-        if (arrayA.Count > arrayB.Count) Console.WriteLine("Right side ran out of items, so inputs are not in the right order");
-        return arrayA.Count <= arrayB.Count ? Result.CorrectOrder : Result.WrongOrder;
     }
 
     enum Result
@@ -109,17 +75,6 @@ class Solution : Solver {
         Continue,
         CorrectOrder,
         WrongOrder,
-    }
-
-    //to delete
-    private static bool IsListForm(string str) => str.StartsWith('[') && str.EndsWith(']');
-
-    //to delete
-    private static void Log(string left, string right, int indent)
-    {
-        if (!DEBUG) return;
-        string tab = new string(' ', indent);
-        Console.WriteLine($"{tab}- Compare {left} vs {right}");
     }
 
     private void ReadInput(string input)
@@ -133,6 +88,13 @@ class Solution : Solver {
             .ToList();
     }
 
+    private IEnumerable<JsonNode> ReadInputPart2(string input)
+    {
+        return input.Split("\n")
+            .Where(x => !string.IsNullOrEmpty(x))
+            .Select(line => JsonNode.Parse(line));
+    }
+
     internal class Pairs
     {
         public JsonNode left;
@@ -144,6 +106,5 @@ class Solution : Solver {
             this.right = JsonNode.Parse(right);
         }
     }
-
     //Perl PCRE recursive Regex that parses this : "[^\\[\\],]*(\\[(?:[^\\[\\]]|(?1))*\\])[^\\[\\],]*(*SKIP)(*F)|,"
 }
