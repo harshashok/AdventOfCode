21d807b9498108b2c7931eb36ee6bcff87a6abde
hashok <hars.ashok@gmail.com>
2023-12-01 22:04:33 -0800

    Day 2022/16 : stubs

---
+++ 2022/Day16/Solution.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode.Y2022.Day16;
+
+[ProblemName("Proboscidea Volcanium")]      
+class Solution : Solver {
+
+    public object PartOne(string input) {
+        return 0;
+    }
+
+    public object PartTwo(string input) {
+        return 0;
+    }
+}
