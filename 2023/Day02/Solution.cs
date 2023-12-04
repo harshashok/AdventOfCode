88727a94460c4af95fb4b8a2020a5a5e8d610aa0
hashok <hars.ashok@gmail.com>
2023-12-03 17:41:54 -0800

    Day 2023/02 : Solution Part 1

--- 2023/Day02/Solution.cs
+++ 2023/Day02/Solution.cs
@@ -4,17 +4,63 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Text;
+using static AdventOfCode.Y2023.Day02.Solution;
 
 namespace AdventOfCode.Y2023.Day02;
 
 [ProblemName("Cube Conundrum")]      
 class Solution : Solver {
+    List<Game> games = new();
 
     public object PartOne(string input) {
-        return 0;
+        ReadInput(input);
+        var cmp = new Cubes(12, 13, 14);
+        return games.Where(game => game.sets.All(g => (g.red <= cmp.red) && (g.green <= cmp.green) && g.blue <= cmp.blue))
+            .Select(game => game.ID)
+            .Sum();
     }
 
     public object PartTwo(string input) {
         return 0;
     }
+
+    void ReadInput(string input)
+    {
+        var aa = input.Split('\n');
+        aa.ToList().ForEach(line => ParseGameSets(line));
+    }
+
+    void ParseGameSets(string line)
+    {
+        var idValues = line.Split(':');
+        Game game = new();
+        game.ID = int.Parse(Regex.Match(idValues[0], @"\d+").Value);
+
+        idValues[1].Split(';')
+                .Select(set => set.Split(','))
+                .ToList()
+                .ForEach(cubes =>
+                {
+                    Dictionary<string, int> cubeMap = new();
+                    foreach (var cube in cubes)
+                    {
+                        var value = cube.Trim().Split();
+                        cubeMap.Add(value[1], int.Parse(value[0]));
+                    }
+                    game.sets.Add(new Cubes(
+                            cubeMap.TryGetValue("red", out int red) ? red : 0,
+                            cubeMap.TryGetValue("green", out int green) ? green : 0,
+                            cubeMap.TryGetValue("blue", out int blue) ? blue : 0
+                        ));
+                });
+        games.Add(game);
+    }
+
+    internal class Game
+    {
+        public int ID;
+        public List<Cubes> sets = new();
+    }
+
+    internal record Cubes (int red, int green, int blue);
 }
