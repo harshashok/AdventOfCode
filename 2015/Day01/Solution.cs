1dc4b5943bf82be442731bc08a8e2ea905774833
hashok <hars.ashok@gmail.com>
2023-12-19 14:44:27 -0800

    Day 2015/01 : Solution part 1 & 2

---
+++ 2015/Day01/Solution.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace AdventOfCode.Y2015.Day01;
+
+[ProblemName("Not Quite Lisp")]      
+class Solution : Solver {
+
+    public object PartOne(string input)
+    {
+        return input.Aggregate(0, (accum, y) => accum + (y == '('? 1 : -1));
+    }
+
+    public object PartTwo(string input)
+    {
+        return GetFloor(input).First(x => x.floor == -1).index;
+    }
+
+    IEnumerable<(int floor, int index)> GetFloor(string input)
+    {
+        var floor = 0;
+        for (int index = 0; index < input.Length; index++)
+        {
+            floor += (input[index] == '(' ? 1 : -1);
+            yield return (floor, index+1);
+        }
+    }
+    
+}
