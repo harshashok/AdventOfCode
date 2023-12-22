42650585ca82c1e395b04fb08ac7858bed96868c
hashok <hars.ashok@gmail.com>
2023-12-22 10:29:28 -0800

    Day 2015/04 : Solution part 1 & 2

---
+++ 2015/Day04/Solution.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Y2015.Day04;
+
+[ProblemName("The Ideal Stocking Stuffer")]      
+class Solution : Solver {
+
+    public object PartOne(string input) => FindMD5(input, "00000");
+
+    public object PartTwo(string input) => FindMD5(input, "000000");
+
+    int FindMD5(string input, string prefix)
+    {
+        var bag = new ConcurrentBag<int>();
+        Parallel.ForEach(
+            Numbers(),           //TSource
+            () => MD5.Create(),  //localInit
+            (i, state, md5) =>   //body
+            {
+                var seed = input + i;
+                var inputBytes = ASCIIEncoding.ASCII.GetBytes(seed);
+                var outputHash = md5.ComputeHash(inputBytes);
+                var result = Convert.ToHexString(outputHash);
+
+                if (result.StartsWith(prefix))
+                {
+                    bag.Add(i);
+                    state.Stop();
+                }
+                return md5;
+                
+            },
+            (_) => {}   //localFinally
+        );
+        return bag.Min();
+    }
+    int CreateMD5(string input)
+    {
+        int iter = 0;
+        using (MD5 md5 = MD5.Create())
+        {
+            while (true)
+            {
+                var seed = input + iter.ToString();
+                var inputBytes = ASCIIEncoding.ASCII.GetBytes(seed);
+                var outputHash = md5.ComputeHash(inputBytes);
+                var result = Convert.ToHexString(outputHash);
+
+                if (result.StartsWith("00000"))
+                {
+                    return iter;
+                }
+
+                iter++;
+            }
+        }
+    }
+
+    IEnumerable<int> Numbers()
+    {
+        for (int i = 0;;i++)
+        {
+            yield return i;
+        }
+    }
+}
