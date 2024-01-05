c663b02fc6621b754553c897618349622790acdf
hashok <hars.ashok@gmail.com>
2024-01-04 17:13:30 -0800

    Day 2015/12 : Solutions

---
+++ 2015/Day12/Solution.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace AdventOfCode.Y2015.Day12;
+
+[ProblemName("JSAbacusFramework.io")]      
+class Solution : Solver {
+
+    public object PartOne(string input) => (from m in Regex.Matches(input, @"-*\d+") select int.Parse(m.Value)).Sum();
+    public object PartTwo(string input) => ParseJson(JsonDocument.Parse(input).RootElement);
+    
+    int ParseJson(JsonElement json)
+    {
+        if (json.ValueKind is JsonValueKind.Number) return json.GetInt32();
+        if (json.ValueKind is JsonValueKind.Array) return json.EnumerateArray().Select(ParseJson).Sum();
+        if (json.ValueKind is JsonValueKind.Object)
+        {
+            if (json.EnumerateObject().Any(x => x.Value.ToString() == "red")) return 0;
+            return json.EnumerateObject().Select(x => ParseJson(x.Value)).Sum();
+        }
+        return 0;
+    }
+}
