4fc537e04a626422daeb2e07dd30d1e0b425779e
hashok <hars.ashok@gmail.com>
2024-06-21 22:32:06 -0700

    Day 2015/20 : Solution Part 2

--- 2015/Day20/Solution.cs
+++ 2015/Day20/Solution.cs
@@ -12,34 +12,33 @@ class Solution : Solver {
 
     public object PartOne(string input) => CalcPresents(houses: 1000000, target: Int32.Parse(input), multiplier: 10);
 
-    public object PartTwo(string input) {
-        return 0;
-    }
+    public object PartTwo(string input) => CalcPresents(houses: 1000000, target: Int32.Parse(input), multiplier: 11, skip: true);
 
-    private int CalcPresents(int houses, int target, int multiplier)
+    private int CalcPresents(int houses, int target, int multiplier, bool skip=false)
     {
         long[] presents = new long [houses+1];
 
         for (int elf = 1; elf <= houses; elf++)
         {
-            int skipCount = elf;
+            int skipCount = 0;
             for (int house = elf; house <= houses;)
             {
                 if (house % elf == 0)
                 {
                     presents[house] += elf * multiplier;
+                    skipCount++;
                 }
-                house += skipCount;
+
+                if (skip && skipCount >= 50) break;
+                house += elf;
             }
         }
-
-
+        
         for (int i = 1; i < presents.Length; i++)
         {
             if (presents[i] >= target)
                 return i;
         }
-
         return -1;
     }
 }
